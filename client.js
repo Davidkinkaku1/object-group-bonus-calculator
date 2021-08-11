62da1812ab34830990ec7a12fbbc6d08ea99230d
David Kinkaku <davidkinkaku1@gmail.com>
2021-08-11 11:05:46 -0500

    updates

--- client.js
+++ client.js
@@ -1,37 +1,67 @@
-const employees = [
-  {
-    name: 'Atticus',
-    employeeNumber: '2405',
-    annualSalary: '47000',
-    reviewRating: 3
-  },
-  {
-    name: 'Jem',
-    employeeNumber: '62347',
-    annualSalary: '63500',
-    reviewRating: 4
-  },
-  {
-    name: 'Scout',
-    employeeNumber: '6243',
-    annualSalary: '74750',
-    reviewRating: 5
-  },
-  {
-    name: 'Robert',
-    employeeNumber: '26835',
-    annualSalary: '66000',
-    reviewRating: 1
-  },
-  {
-    name: 'Mayella',
-    employeeNumber: '89068',
-    annualSalary: '35000',
-    reviewRating: 1
-  }
-];
+const employees = [{
+        name: 'Atticus',
+        employeeNumber: '2405',
+        annualSalary: '47000',
+        reviewRating: 3
+    },
+    {
+        name: 'Jem',
+        employeeNumber: '62347',
+        annualSalary: '63500',
+        reviewRating: 4
+    },
+    {
+        name: 'Scout',
+        employeeNumber: '6243',
+        annualSalary: '74750',
+        reviewRating: 5
+    },
+    {
+        name: 'Robert',
+        employeeNumber: '26835',
+        annualSalary: '66000',
+        reviewRating: 1
+    },
+    {
+        name: 'Mayella',
+        employeeNumber: '89068',
+        annualSalary: '35000',
+        reviewRating: 1
+    }
+]; //end employees object
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
+
+//function is getting name value from the employee object and creating a new function
+function bonusCalc(employeeId) {
+    let empBonus = {
+        name: employeeId.name,
+        bonusPercentage: 0,
+        totalCompensation: 0,
+        totalBonus: 0
+    }
+
+    return empBonus;
+} // end of bonuCalc
+console.log(bonusCalc(employees));
+
+
+//Looping through the array to get all the employees info so we don't have to indvidually console.log them
+for (let i = 0; i < employees.length; i++) {
+
+    console.log(bonusCalc(employees[i]));
+}
+
+
+
+
+
+
+
+
+
+
+
 
 // Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
 // This problem is massive! Break the problem down. Use the debugger.
@@ -40,4 +70,4 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+console.log(employees);
