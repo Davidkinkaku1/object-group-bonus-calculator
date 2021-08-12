333de06ac00b8bf180fbdfa21fc8a1a5eceeb962
David Kinkaku <davidkinkaku1@gmail.com>
2021-08-11 19:25:13 -0500

    final updates

--- client.js
+++ client.js
@@ -36,30 +36,50 @@ const employees = [{
 function bonusCalc(employeeId) {
     let empBonus = {
         name: employeeId.name,
-        bonusPercentage: 0,
-        totalCompensation: 0,
-        totalBonus: 0
+        bonusPercentage: 0.0,
+        totalBonus: 0,
+        totalCompensation: Number(employeeId.annualSalary),
     }
+
+    if (employeeId.reviewRating <= 2) {
+        empBonus.bonusPercentage = 0.0;
+    } else if (employeeId.reviewRating === 3) {
+        empBonus.bonusPercentage = 0.4;
+    } else if (employeeId.reviewRating === 4) {
+        empBonus.bonusPercentage = 0.06;
+    } else if (employeeId.reviewRating === 5) {
+        empBonus.bonusPercentage = 0.10;
+    } else {
+        // greater than 5? unsupported (do nothing)
+    }
+
+
+    // Check employee ID length
+    if (employeeId.employeeNumber.length <= 4) {
+        // add 5% to the current bonus   
+        empBonus.bonusPercent += 0.05;
+    }
+    if (employeeId.annualSalary > 65000) {
+        empBonus.bonusPercent -= 0.01;
+    }
+    if (empBonus.bonusPercentage > 0.13) {
+        empBonus.bonusPercent = 0.13;
+    } else if (empBonus.bonusPercent < 0) {
+        empBonus.bonusPercent = 0.0;
+    }
+
+
+
+
+
+
 
     return empBonus;
 } // end of bonuCalc
-console.log(bonusCalc(employees));
+console.log(employees);
 
 
 //Looping through the array to get all the employees info so we don't have to indvidually console.log them
-for (let i = 0; i < employees.length; i++) {
-
-    console.log(bonusCalc(employees[i]));
-}
-
-
-
-
-
-
-
-
-
 
 
 
@@ -70,4 +90,18 @@ for (let i = 0; i < employees.length; i++) {
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log(employees);
+for (let emp of employees) {
+    console.log(`Processing bonus for: ${emp.name}`);
+    const bonus = bonusCalc(emp);
+    console.log(`Bonus Percent: ${emp.reviewRating}`);
+    console.log(`Total Bonus: ${bonus.bonusPercent * 100}`);
+    console.log(`Total comp: ${bonus.totalCompensation}`);
+}
+
+
+// test calculations of one employees
+
+let atticus = employees[0];
+let atticusBonus = bonusCalc(atticus);
+console.log(`Atticus should have a bonus of .09: ${atticusBonus.bonusPercent}`);
+console.log(`Atticus should have a total compensation of 47000: ${atticusBonus.totalCompensation}`);
